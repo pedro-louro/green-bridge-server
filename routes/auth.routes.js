948a829550906fdaf8c39be38709fc425bc642d4
pedro-louro <pedro.louro1@live.com.pt>
2023-07-27 18:47:40 +0100

    added address to the user signup route

--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -20,7 +20,7 @@ const saltRounds = 10;
 
 // POST /auth/signup  - Creates a new user in the database
 router.post('/signup', (req, res, next) => {
-  const { email, password, name } = req.body;
+  const { email, password, name, address } = req.body;
 
   // Check if email or password or name are provided as empty strings
   if (email === '' || password === '' || name === '') {
@@ -60,12 +60,12 @@ router.post('/signup', (req, res, next) => {
 
       // Create the new user in the database
       // We return a pending promise, which allows us to chain another `then`
-      return User.create({ email, password: hashedPassword, name });
+      return User.create({ email, password: hashedPassword, name, address });
     })
     .then(createdUser => {
       // Deconstruct the newly created user object to omit the password
       // We should never expose passwords publicly
-      const { email, name, _id } = createdUser;
+      const { email, name, _id, address } = createdUser;
 
       // Create a new object that doesn't expose the password
       const user = { email, name, _id };
